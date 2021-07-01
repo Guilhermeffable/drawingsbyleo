19f81d412bdbc32493d232fe3ed1155ef0481a1f
Guilherme Luís <guilhermeffable@gmail.com>
2021-07-01 16:47:12 +0100

    Cleaned some unused Code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux';
 import galleryReducer from './galleryReducer';
-import firebaseReducer from 'react-redux-firebase';
-import { createFirestoreInstance, firestoreReducer } from 'redux-firestore';
 
 export default combineReducers({
-    gallery: galleryReducer,
-    firebase: firebaseReducer,
-    firestore: firestoreReducer
+    gallery: galleryReducer
 });
