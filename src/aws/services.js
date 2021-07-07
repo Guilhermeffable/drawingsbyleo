cc18be3b64994a1f40e24754e6073d5b7b6e24d0
Guilherme Luís <guilhermeffable@gmail.com>
2021-07-07 14:58:29 +0100

    final steps for deployment

--- src/aws/services.js
+++ src/aws/services.js
@@ -38,24 +38,22 @@ if (process.env.NODE_ENV !== 'production') {
     console.log(process.env.AWS_USER_FILES_S3_BUCKET);
 
     config = {
-        'aws_project_region': process.env.REACT_APP_AWS_PROJECT_REGION,
-        'aws_appsync_graphqlEndpoint':
-            process.env.REACT_APP_AWS_APPSYNC_GRAPHQLENDPOINT,
-        'aws_appsync_region': process.env.REACT_APP_AWS_APPSYNC_REGION,
+        'aws_project_region': process.env.AWS_PROJECT_REGION,
+        'aws_appsync_graphqlEndpoint': process.env.AWS_APPSYNC_GRAPHQLENDPOINT,
+        'aws_appsync_region': process.env.AWS_APPSYNC_REGION,
         'aws_appsync_authenticationType':
-            process.env.REACT_APP_AWS_APPSYNC_AUTHENTICATIONTYPE,
-        'aws_appsync_apiKey': process.env.REACT_APP_AWS_APPSYNC_APIKEY,
+            process.env.AWS_APPSYNC_AUTHENTICATIONTYPE,
+        'aws_appsync_apiKey': process.env.AWS_APPSYNC_APIKEY,
         'aws_cognito_identity_pool_id':
-            process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
-        'aws_cognito_region': process.env.REACT_APP_AWS_COGNITO_REGION,
-        'aws_user_pools_id': process.env.REACT_APP_AWS_USER_POOLS_ID,
+            process.env.AWS_COGNITO_IDENTITY_POOL_ID,
+        'aws_cognito_region': process.env.AWS_COGNITO_REGION,
+        'aws_user_pools_id': process.env.AWS_USER_POOLS_ID,
         'aws_user_pools_web_client_id':
-            process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
+            process.env.AWS_USER_POOLS_WEB_CLIENT_ID,
         ' oauth': {},
-        'aws_user_files_s3_bucket':
-            process.env.REACT_APP_AWS_USER_FILES_S3_BUCKET,
+        'aws_user_files_s3_bucket': process.env.AWS_USER_FILES_S3_BUCKET,
         'aws_user_files_s3_bucket_region':
-            process.env.REACT_APP_AWS_USER_FILES_S3_BUCKET_REGION
+            process.env.AWS_USER_FILES_S3_BUCKET_REGION
     };
 }
 
